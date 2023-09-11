d6ffaafdf35f26a49158df48cf06924a5e3a648f
Fajar <kucaykojek@gmail.com>
2023-09-12 03:28:59 +0700

    chore: update search page spacing

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,7 +3,7 @@ import SearchResult from '@/components/Search/SearchResult'
 export default function SearchPage() {
   return (
     <div
-      className="container pt-36 pb-10 min-h-[calc(100vh-3rem)]"
+      className="container pt-36 pb-10 px-4 min-h-[calc(100vh-3rem)]"
       data-testid="search-result"
     >
       <SearchResult />
